0980bf2863aa2512c6cd4bef4d9055537fb4648b
rivaldoDeveloper <rivaldo.website@gmail.com>
2026-06-28 16:27:56 -0300

    feat: creating projects

---
+++ payment-batch/src/main/java/com/bbv/payment_batch/PaymentBatchApplication.java
@@ -0,0 +1,13 @@
+package com.bbv.payment_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentBatchApplication.class, args);
+	}
+
+}
---
+++ shared-core-api/src/main/java/com/bbv/shared_core_api/SharedCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.bbv.shared_core_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedCoreApiApplication.class, args);
+	}
+
+}
